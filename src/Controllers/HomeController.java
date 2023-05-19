a2e03666e4dd097bb16318863e7a03472fda80c0
POM-MOUYLANG <126035070+POMMOUYLANG@users.noreply.github.com>
2023-05-19 14:04:09 +0700

    Home_Page.fxml

---
+++ src/Controllers/HomeController.java
@@ -0,0 +1,23 @@
+package Controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class HomeController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
+
+
+
