b72e02141dc5b351f1fb5ad318b726b74d52e3b9
selytheng <kh.lytheng@gmail.com>
2023-05-19 14:31:38 +0700

    aboutus

---
+++ src/Controllers/maincontroller.java
@@ -0,0 +1,31 @@
+package Controllers;
+
+import javax.swing.Action;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class maincontroller {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    public void switchtoaboutus(ActionEvent event){
+        Parent root= FXMLLoader.load(getClass().getResource("About_us.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchtocategories(ActionEvent event){
+        
+    }
+    public void switchtologin(ActionEvent event){
+        
+    }
+    
+}
