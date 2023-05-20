729e3172775fa953276eb843687a728e382e2aa6
POM-MOUYLANG <126035070+POMMOUYLANG@users.noreply.github.com>
2023-05-20 14:10:21 +0700

    All

---
+++ src/Controllers/AdminHomePage.java
@@ -0,0 +1,31 @@
+package Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class AdminHomePage {
+
+    @FXML
+    private Button Homeid;
+
+    @FXML
+    private Button aboutusid;
+
+    @FXML
+    private Button aboutusid1;
+
+    @FXML
+    private Button login;
+
+    @FXML
+    void switchtoaboutus(ActionEvent event) {
+
+    }
+
+    @FXML
+    void switchtologin(ActionEvent event) {
+
+    }
+
+}
