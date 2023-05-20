96d0104cd13dc293f99a12f6f372136530861f72
selytheng <126034309+selytheng@users.noreply.github.com>
2023-05-20 15:07:47 +0700

    controller changes

--- src/Controllers/HomeController.java
+++ src/Controllers/HomeController.java
@@ -1,23 +1,41 @@
 package Controllers;
 
-import java.net.URL;
-import java.util.ResourceBundle;
+import javax.swing.Action;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 
-public class HomeController {
+import java.io.IOException;
 
-    @FXML
-    private ResourceBundle resources;
-
-    @FXML
-    private URL location;
-
-    @FXML
-    void initialize() {
-
+public class homecontroller {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    public void switchtoaboutus(ActionEvent event) throws IOException {
+        Parent root= FXMLLoader.load(getClass().getResource("AboutUS.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
-
+    public void switchtocategories(ActionEvent event)throws IOException {
+        Parent root= FXMLLoader.load(getClass().getResource("Categories.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchtologin(ActionEvent event) throws IOException {
+        Parent root= FXMLLoader.load(getClass().getResource("user_login.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    
 }
-
-
-
