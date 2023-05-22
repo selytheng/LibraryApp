1755fc1fff5a45aec1cfdba598582c1764ca08df
selytheng <126034309+selytheng@users.noreply.github.com>
2023-05-22 16:34:18 +0700

    login

--- src/Controllers/LoginController.java
+++ src/Controllers/LoginController.java
@@ -1,15 +1,22 @@
 package Controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.awt.*;
 import java.io.IOException;
 
 public class LoginController {
+    @FXML
+    TextField userid;
     private Stage stage;
     private Scene scene;
     private Parent root;
@@ -21,10 +28,14 @@ public class LoginController {
         stage.show();
     }
     public void login(ActionEvent event) throws IOException {
-        Parent root= FXMLLoader.load(getClass().getResource("HomePage.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        String username = userid.getText();
+        if (username=="admin"){
+            Parent root= FXMLLoader.load(getClass().getResource("AdminHomePage.fxml"));
+            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+
     }
 }
