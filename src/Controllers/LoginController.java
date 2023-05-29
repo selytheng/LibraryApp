2d8071fc917cdcec4ee5621f458546f0f4827c10
POM-MOUYLANG <126035070+POMMOUYLANG@users.noreply.github.com>
2023-05-29 11:11:08 +0700

    errorfx

--- src/Controllers/LoginController.java
+++ src/Controllers/LoginController.java
@@ -20,25 +20,35 @@ public class LoginController {
     TextField userid;
     @FXML
     TextField passid;
-    private Button button;
     private Stage stage;
     private Scene scene;
-    private Parent root;
+
     public void switchtohome(ActionEvent event) throws IOException {
-        Parent root= FXMLLoader.load(getClass().getResource("HomePage.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
 
     public void login(ActionEvent event) throws IOException {
-        checkLogin();
-    }
-    public void checkLogin() throws IOException {
-        Main m = new Main();
-        if(userid.getText().toString().equals("admin") && passid.getText().toString().equals("123")){
-            m.changescene("AdminHomePage.fxml");
+        String id = userid.getText();
+        String pass = passid.getText();
+        if (id.equals("admin") && pass.equals("123")) {
+            // FXMLLoader loader = new FXMLLoader(getClass().getResource("AdminHomePage.fxml"));
+            // Parent welcomeParent = loader.load();
+            // Scene welcomeScene = new Scene(welcomeParent);
+            // Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            // window.setScene(welcomeScene);
+
+            Parent root = FXMLLoader.load(getClass().getResource("AdminHomePage.fxml"));
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+            // System.out.println("kdkd");
+        } else {
+            System.out.println("Incorrect username or password");
         }
     }
 
