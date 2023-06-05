794d618580d307ff57175fa7828dc9d8dc0e8d1b
selytheng <126034309+selytheng@users.noreply.github.com>
2023-06-05 12:14:25 +0700

    controller

--- src/Controllers/AdminHomePage.java
+++ src/Controllers/AdminHomePage.java
@@ -1,31 +1,48 @@
 package Controllers;
 
+import javax.swing.Action;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+import java.io.IOException;
 
 public class AdminHomePage {
-
-    @FXML
-    private Button Homeid;
-
-    @FXML
-    private Button aboutusid;
-
-    @FXML
-    private Button aboutusid1;
-
-    @FXML
-    private Button login;
-
-    @FXML
-    void switchtoaboutus(ActionEvent event) {
-
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    public void switchtohome(ActionEvent event) throws IOException {
+        Parent root= FXMLLoader.load(getClass().getResource("AdminHomePage.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
-    @FXML
-    void switchtologin(ActionEvent event) {
-
+    public void switchtoaboutus(ActionEvent event) throws IOException {
+        Parent root= FXMLLoader.load(getClass().getResource("AboutUS.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchtocategories(ActionEvent event)throws IOException {
+        Parent root= FXMLLoader.load(getClass().getResource("ListBook.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchtoadmin(ActionEvent event) throws IOException {
+        Parent root= FXMLLoader.load(getClass().getResource("AdminList.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
